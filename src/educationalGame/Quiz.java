acb202a24dde6be1038f1edeeddfadf55aa5a4f4
Anas <anasbouzina02@gmail.com>
2023-03-17 01:22:24 +0100

    third commit

--- src/educationalGame/Quiz.java
+++ src/educationalGame/Quiz.java
@@ -86,7 +86,10 @@ public class Quiz implements ActionListener{
             'C',
             'D',
             'B',
-            'B'
+            'B',
+            'C',
+            'A',
+            'A'
 							};
 	char guess;
 	char answer;
